5fd699be3a1a17a6bcdb2339c3006eef535de948
David Arias <davidalexarias2@gmail.com>
2018-04-24 17:25:53 -0700

    up to date after setting up backend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
 import { Geolocation } from '@ionic-native/geolocation';
 
 declare var google;
@@ -10,50 +9,40 @@ declare var google;
   templateUrl: 'home.html'
 })
 export class HomePage {
- 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
- 
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public _geo: Geolocation) {
- 
-  }
- 
+              public _geo: Geolocation) {}
+
   ionViewDidLoad(){
     this.loadMap();
   }
  
   loadMap(){
- 
     this._geo.getCurrentPosition().then((position) => {
- 
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      console.log(latLng)
+      let latLng = new google.maps.LatLng(position.coords.latitude, 
+                                          position.coords.longitude);
+      console.log("hello", latLng)
       let mapOptions = {
         center: latLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
- 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
     }, (err) => {
       console.log(err);
     });
-    
   }
   
   addInfoWindow(marker, content){
- 
     let infoWindow = new google.maps.InfoWindow({
       content: content
     });
-   
     google.maps.event.addListener(marker, 'click', () => {
       infoWindow.open(this.map, marker);
     });
- 
   }
   
   addMarker(){
@@ -62,9 +51,7 @@ export class HomePage {
       animation: google.maps.Animation.DROP,
       position: this.map.getCenter()
     });
- 
     let content = "<h4>Information!</h4>";         
- 
     this.addInfoWindow(marker, content);
   }
 }
