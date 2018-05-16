1b23b2008c8e495f95fe376d9eb0ecc9e7978c19
David Arias <davidalexarias2@gmail.com>
2018-05-16 14:29:43 -0700

    removed bsubject

--- src/pages/faves/faves.ts
+++ src/pages/faves/faves.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
 @Component({
   selector: 'page-faves',
   templateUrl: 'faves.html',
